d3e1bf55f9821bbc7b4645bc1c8bfc12ea44b46c
Evgeniy Andreev (gsomix) <gsomix@gmail.com>
2018-11-29 02:08:52 +0400

    Add Shortest intro to compiler design

---
+++ shortest-intro-to-compiler-design.fsx
@@ -0,0 +1,42 @@
+// Original:
+// https://github.com/true-grue/Compiler-Development/wiki/Кратчайшее-введение-в-создание-компилятора-(Python)
+
+open System
+
+let [<Literal>] ST_SIZE = 100
+let [<Literal>] C_CODE = """#include <stdio.h>
+int main(int argc, char** argv) {{ 
+int st[{0}], sp = 0;
+{1}
+printf("%d\n", st[sp - 1]);
+return 0;
+}}"""
+
+type IR =
+    | Push of int
+    | Op of string
+
+let scan (source: string) =
+    let tokens = source.Split [|' '|]
+    [ for x in tokens -> 
+        if Char.IsDigit x.[0] then
+            Push (int(x))
+        else
+            Op x ]
+
+let trans ir =
+    [ for instr in ir do
+        match instr with
+        | Push value -> 
+            yield sprintf "st[sp] = %d;" value
+            yield "sp += 1;"
+        | Op sym -> 
+            yield sprintf "st[sp - 2] = st[sp - 2] %s st[sp - 1];" sym
+            yield "sp -= 1;" 
+    ] |> String.concat "\n"
+
+let rpnToC source = 
+    let code = source |> scan |> trans
+    String.Format(C_CODE, ST_SIZE, code)
+
+printfn "%s" (rpnToC "2 2 + 3 -") // 2 + 2 - 3
